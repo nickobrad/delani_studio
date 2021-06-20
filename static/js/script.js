40d59297f5186a59df4fb8b75238f94fe6713d13
omondi <nicholasomosh@gmail.com>
2021-06-20 10:04:14 +0300

    Pushing now

--- static/js/script.js
+++ static/js/script.js
@@ -33,49 +33,49 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $("#work1 img").hover(function(){
+    $("#work1").hover(function(){
         $(".center1").toggle();
     });
 });
 
 $(document).ready(function(){
-    $("#work2 img").hover(function(){
+    $("#work2").hover(function(){
         $(".center2").toggle();
     });
 });
 
 $(document).ready(function(){
-    $("#work3 img").hover(function(){
+    $("#work3").hover(function(){
         $(".center3").toggle();
     });
 });
 
 $(document).ready(function(){
-    $("#work4 img").hover(function(){
+    $("#work4").hover(function(){
         $(".center4").toggle();
     });
 });
 
 $(document).ready(function(){
-    $("#work5 img").hover(function(){
+    $("#work5").hover(function(){
         $(".center5").toggle();
     });
 });
 
 $(document).ready(function(){
-    $("#work6 img").hover(function(){
+    $("#work6").hover(function(){
         $(".center6").toggle();
     });
 });
 
 $(document).ready(function(){
-    $("#work7 img").hover(function(){
+    $("#work7").hover(function(){
         $(".center7").toggle();
     });
 });
 
 $(document).ready(function(){
-    $("#work8 img").hover(function(){
+    $("#work8").hover(function(){
         $(".center8").toggle();
     });
 });
